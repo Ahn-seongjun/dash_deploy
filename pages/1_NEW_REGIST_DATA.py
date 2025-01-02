f685885fa3937d408f9e5efcd68d62cbf7ee254b
Ahn-seongjun <aklsg@naver.com>
2025-01-02 17:18:35 +0900

    change4

--- pages/1_NEW_REGIST_DATA.py
+++ pages/1_NEW_REGIST_DATA.py
@@ -3,44 +3,44 @@ import pandas as pd
 from folium.plugins import HeatMap
 from streamlit_folium import st_folium
 import folium
-
+import base64
 
 st.set_page_config(page_title= "New Regist Data", layout="wide", initial_sidebar_state="auto")
-df = pd.read_csv('../데이터/data/22년신규등록데이터.csv')
-
-st.sidebar.write(df)
-
-
-
-
-
-heat_df = df[['위도', '경도']]
-heat_data = [[row['위도'],row['경도']] for index, row in heat_df.iterrows()]
-df3 = df.groupby(['USE_STRNGHLD_ADRES_NM', '위도', '경도']).count()
-df3.reset_index(inplace=True)
-
-st.title('CAR_REG Dashboard')
-chart = st.selectbox("차트종류", ['bubble', 'heat'])
-kr = folium.Map(location=[36, 128.5], zoom_start=7, tiles='openStreetMap')
-if chart =='heat':
-    HeatMap(heat_data).add_to(kr)
-else :
-    for i in range(0, len(df3)):
-        latitude = df3.iloc[i]['위도']
-        longitude = df3.iloc[i]['경도']
-        location = (latitude, longitude)
-        folium.CircleMarker(location, radius=df3.iloc[i]['AGE'] / 1000, color='#3186cc', fill_color='#3186cc',
-                            popup=df3.iloc[i]['USE_STRNGHLD_ADRES_NM']).add_to(kr)
-
-    folium.LayerControl(collapsed=False).add_to(kr)
-st_data = st_folium(kr, width = 725)
-
-
-
-left_column, right_column = st.columns([1.5,2.5])
-with left_column:
-    st.subheader('Dataset')
-    st.dataframe(df)
+# df = pd.read_csv('../데이터/data/22년신규등록데이터.csv')
+#
+# st.sidebar.write(df)
+#
+#
+#
+#
+#
+# heat_df = df[['위도', '경도']]
+# heat_data = [[row['위도'],row['경도']] for index, row in heat_df.iterrows()]
+# df3 = df.groupby(['USE_STRNGHLD_ADRES_NM', '위도', '경도']).count()
+# df3.reset_index(inplace=True)
+#
+# st.title('CAR_REG Dashboard')
+# chart = st.selectbox("차트종류", ['bubble', 'heat'])
+# kr = folium.Map(location=[36, 128.5], zoom_start=7, tiles='openStreetMap')
+# if chart =='heat':
+#     HeatMap(heat_data).add_to(kr)
+# else :
+#     for i in range(0, len(df3)):
+#         latitude = df3.iloc[i]['위도']
+#         longitude = df3.iloc[i]['경도']
+#         location = (latitude, longitude)
+#         folium.CircleMarker(location, radius=df3.iloc[i]['AGE'] / 1000, color='#3186cc', fill_color='#3186cc',
+#                             popup=df3.iloc[i]['USE_STRNGHLD_ADRES_NM']).add_to(kr)
+#
+#     folium.LayerControl(collapsed=False).add_to(kr)
+# st_data = st_folium(kr, width = 725)
+#
+#
+#
+# left_column, right_column = st.columns([1.5,2.5])
+# with left_column:
+#     st.subheader('Dataset')
+#     st.dataframe(df)
 
 with open('./assets/carcharts.png', "rb") as f:
     data = f.read()
